cef8ac994725fad8856252d2380e1067fe0c95c1
nicolasnarvaez <nicolas_narvaez@protonmail.ch>
2020-04-25 00:54:42 -0400

    Add: Shader.js_compile

--- src/NEngine/GLNSLCompiler/Shader.js
+++ src/NEngine/GLNSLCompiler/Shader.js
@@ -23,49 +23,62 @@ function Shader(opts) {
 	this.program = null
 }
 
-Shader.prototype = {
+Object.assign(Shader.prototype, {
+	/**
+	 * Compiles a js function declaring this.js_variables in its context
+	 * appending it in the form 'entry_key = fn_argument || entry_value'.
+	 * Added keys can be added as arguments or will fallback to the given
+	 * default values. </br></br>
+	 * @param  {string} src  - js src code to compile
+	 * @param  {object} vars - key-default_value vars definition
+	 * @return {Function}      - compiled js function
+	 */
+	// TODO generalize to WASM
+	js_compile: function js_compile(src, vars = this.js_variables) {
+		
+		var keys = Object.keys(vars)
+		var args = keys.join(',')
+		var body = keys.map(function(e) {
+				e + '=' + e + '||' + JSON.stringify(vars[e]) + ';\n'
+			}).join() + '\n'+
+			'return ' + src
+			
+		var res_fn
+		try {
+			res_fn = Function(args, body)
+		}
+		catch(error) {
+			console.error('Error compiling fn in shader: ')
+			console.log(src)
+			throw error
+		}
+		
+		return res_fn
+	},
 	/**
 	 * @memberof NEngine.GLNSLCompiler.Shader
 	 * @method js_execute
-	 * @desc executes the src str adding code_tree.shader.js_variables
-	 * to its context. </br></br>
+	 * @desc gets the executed source over the given args. </br></br>
 	 * TODO currently only one line expression evaluation functions
 	 * allowed. </br>
 	 * TODO develop arbitrarily complex js (possible using function
 	 * expression). </br>
-	 * @arg {string}
+	 * @arg {string} source - The source code to parse
+	 * @
 	 */
-	js_execute: function js_execute(src, opts) {
-		var f, args, keys , body, res, vars
-
-		res = {}
-
-		vars = this.js_variables
-		keys = Object.keys(vars)
-
-		//TODO(maybeready) sending js_variables by parameters allows updating
-		//results withouth recompiling function
-		args = keys.join(',')
-		body = 'var '+
-			keys.map(function(e){
-				e+'='+e+'||'+vars[e]
-			}).join(',') + ';'+
-			'return '+src
-
+	js_execute: function js_execute(src, args) {
+		var n = this.js_compile(src)
+		var res
+		
 		try {
-			res.f = f = Function(args, body)
-			try {
-				res.res = f()
-			}
-			catch(e) {
-				res.err = 'Exception executing js function: \n\n'+src+
-					'\n\nException: '+e
-			}
+			res = fn.apply(null, args)
 		}
-		catch(e) {
-			res.err = 'Exception compiling shader function: \n\n'+src+
-				'\n\nException: '+e
+		catch(error) {
+			console.error('Exception executing js function: ')
+			console.log(src, args)
+			throw error
 		}
+		
 		return res
 	},
 	/**
@@ -75,6 +88,6 @@ Shader.prototype = {
 		this.code_tree = new CodeTree(this.src, this.js_variables)
 		return code_tree.translate()
 	}
-}
+})
 return Shader
 })()
